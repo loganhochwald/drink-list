b64354c64cca76d3c062ba0c295c0ddc95bc21f7
Logan Hochwald <loganhochwald@gmail.com>
2025-06-25 13:56:23 -0500

    feat: install eslint and prettier configs

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,15 @@
-// https://docs.expo.dev/guides/using-eslint/
 const { defineConfig } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
+const eslintPluginPrettierRecommended = require('eslint-plugin-prettier/recommended');
 
 module.exports = defineConfig([
   expoConfig,
+  {
+    rules: {
+      'prettier/prettier': ['error', { singleQuote: true }],
+    },
+  },
+  eslintPluginPrettierRecommended,
   {
     ignores: ['dist/*'],
   },
